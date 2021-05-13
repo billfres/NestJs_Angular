1c8816f3809ddcb73ac5c0e48575845cdef42332
fresnel <ngomafresnel@gmail.com>
2021-05-13 03:03:48 +0000

    Correction de la pagination

--- src/todo/dto/get-paginated-todo.dto.ts
+++ src/todo/dto/get-paginated-todo.dto.ts
@@ -1,12 +1,15 @@
+import { Type } from "class-transformer";
 import { IsNumber, IsOptional } from "class-validator";
 
 export class GetPaginatedTodoDto{
 
     @IsNumber()
     @IsOptional()
+    @Type( ()=> Number)
     page: number;
 
     @IsNumber()
     @IsOptional()
+    @Type( ()=> Number)
     item: number;
 }
