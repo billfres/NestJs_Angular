b2a2beb5936745bad4491dc9e069d61b63e7be23
fresnel <ngomafresnel@gmail.com>
2021-05-14 23:23:29 +0000

    middleware global ou prioritaire dans main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { logger } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  /*app.use(//logger
-    () => {
+  app.use(logger
+    /*() => {
       (req: Request, res: Response, next) => {
         console.log('Middleware form app.use');
         //next();
       }
-    }
-  )*/
+    }*/
+  )
   app.useGlobalPipes(new ValidationPipe({
     transform: true,//transformation des elts en le type voulu
     whitelist : true, //blocage des elts non souhaités
