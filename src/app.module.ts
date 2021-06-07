2d824ddccd4d467c63ff7aac953529783f966fd9
fresnel <ngomafresnel@gmail.com>
2021-06-08 00:53:58 +0100

    Configuration de la DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,26 @@ import { FirstMiddleware } from './middlewares/first.middleware';
 import { logger } from './middlewares/logger.middleware';
 import { TodoModule } from './todo/todo.module';
 
+import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+dotenv.config();
+
 @Module({
   imports: [
     TodoModule,
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
     })
   ],
   controllers: [AppController],
