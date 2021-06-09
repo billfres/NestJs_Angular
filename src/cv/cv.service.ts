bf20e4a36e09853d79e699a6850aa46ad7097087
fresnel <ngomafresnel@gmail.com>
2021-06-09 19:26:24 +0100

    Injection de l'entité cv dans le module cv

--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CvEntity } from './entities/cv.entity';
 
 @Injectable()
-export class CvService {}
+export class CvService {
+   
+}
