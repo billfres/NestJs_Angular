27a55abc26f0758dd11da0b7013e1f5af03ff0b5
fresnel <ngomafresnel@gmail.com>
2021-06-11 01:04:31 +0100

    TypeOrm Respository save a cv

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CvService } from './cv.service';
+import { AddCvDto } from './dto/Add-cv.dto';
 import { CvEntity } from './entities/cv.entity';
 
 @Controller('cv')
@@ -12,5 +13,12 @@ export class CvController {
     @Get()
     async getAllCvs(): Promise<CvEntity[]>{
         return await this.cvService.getCvs();
+    }
+
+    @Post()
+    async addCv(
+        @Body() addCvDto: AddCvDto
+    ): Promise<CvEntity>{
+        return await this.cvService.addCv(addCvDto);
     }
 }
--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AddCvDto } from './dto/Add-cv.dto';
 import { CvEntity } from './entities/cv.entity';
 
 @Injectable()
@@ -13,5 +14,10 @@ export class CvService {
 
     async getCvs(): Promise<CvEntity[]>{
         return await this.cvRespository.find();
+    }
+
+    async addCv(cv: AddCvDto): Promise<CvEntity>{
+
+        return await this.cvRespository.save(cv);
     }
 }
