1223e7fd70e492492d712941fe2dc75a6979b5ac
fresnel <ngomafresnel@gmail.com>
2021-09-30 03:27:53 +0100

    Ajout de la methode register

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserSubscribeDto } from './dto/user-subscribe.dto';
+import { UserEntity } from './entities/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private userService: UserService
+    ){}
+
+    @Post()
+    register(
+        @Body() userData: UserSubscribeDto
+    ): Promise<Partial<UserEntity>>{
+        return this.userService.register(userData);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UserSubscribeDto } from './dto/user-subscribe.dto';
+import { UserEntity } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private userRepository: Repository<UserEntity>
+    ){}
+
+    async register(userData : UserSubscribeDto): Promise<Partial<UserEntity>>{
+        const user = this.userRepository.create({
+            ...userData
+        });
+        user.salt = await bcrypt.genSalt();
+        user.password = await bcrypt.hash(user.password, user.salt);
+
+        try {
+            await this.userRepository.save(user);
+        } catch (e) {
+            throw new ConflictException(`Le username et le password doivent être unique`);
+        }
+
+        return{
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+          };
+    }
+}
